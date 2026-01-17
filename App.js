3ca943af7df9baef7d9a7c1f64089b6d9ae12e66
Aarushi Singh <12345678+The-AarushiSingh@users.noreply.github.com>
2026-01-17 18:44:35 +0530

    Day 1: React createElement and root

---
+++ App.js
@@ -0,0 +1,36 @@
+//createElement -> takes 3 arguments (which tag, object/attributes, what value/children)
+//props= attr + children
+//createRoot
+//render
+
+/*
+<div id="parent">
+   <div id="child1">
+     <h1>I am H1 </h1>
+     <h2> Ism H2 </h2>
+    </div>
+    <div id="child2">
+     <h1>I am H1 </h1>
+     <h2> Ism H2 </h2>
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "h1" }, "I am h1"),
+      React.createElement("h2", { id: "h2" }, "I am h2"),
+    ]),
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "h1" }, "I am h1"),
+      React.createElement("h2", { id: "h2" }, "I am h2"),
+    ]),
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
